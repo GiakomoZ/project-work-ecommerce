6e23adb5f21fdb8fed29273fcaa9dbd4cfd8e426
March222 <marcello.belli.stud@ispascalcomandini.it>
2024-06-06 11:59:10 +0200

    Shop spostato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ListaProdottiComponent } from './components/lista-prodotti/lista-prodotti.component';
 
 export const routes: Routes = [
     { path: "", component: HomepageComponent },
+    { path: "shop", component: ListaProdottiComponent },
     {path:"**", component:NotfoundComponent}
 ];
