ad37580c64f75a3483b20e598d8461b0f3a41c0e
GiakomoZ <85363938+GiakomoZ@users.noreply.github.com>
2024-06-06 10:39:49 +0200

    Aggiunti service per categorie e prodotti

--- src/app/services/prodotti.service.ts
+++ src/app/services/prodotti.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Prodotto } from '../models/prodotto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdottiService {
 
-  constructor() { }
+  private apiUrl = 'https://example.com/api/products'; // Sostituisci con l'URL reale della tua API
+
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+     return this.http.get<Prodotto[]>(this.apiUrl);
+  }
 }
