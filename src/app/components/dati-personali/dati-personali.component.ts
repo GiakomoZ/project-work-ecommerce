4ebb57595ef3819d10b448ffc1d2ae4d592492c9
diego6 <giovanninidiego06@gmail.com>
2024-06-06 11:27:59 +0200

    shop

---
+++ src/app/components/dati-personali/dati-personali.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dati-personali',
+  standalone: true,
+  imports: [],
+  templateUrl: './dati-personali.component.html',
+  styleUrl: './dati-personali.component.css'
+})
+export class DatiPersonaliComponent {
+
+}
