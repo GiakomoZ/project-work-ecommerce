849b8fd24f05312716cb3e27196ce264abb36034
GiakomoZ <85363938+GiakomoZ@users.noreply.github.com>
2024-06-06 15:07:42 +0200

    Coponentizzate le card categoria

--- src/app/components/lista-categorie/lista-categorie.component.ts
+++ src/app/components/lista-categorie/lista-categorie.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Categoria } from '../../models/categoria';
 import { CategorieService } from '../../services/categorie.service';
 import { CommonModule } from '@angular/common';
+import { CardCategoriaComponent } from '../card-categoria/card-categoria.component';
 
 @Component({
 	selector: 'app-lista-categorie',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, CardCategoriaComponent],
 	templateUrl: './lista-categorie.component.html',
 	styleUrl: './lista-categorie.component.css',
 })
