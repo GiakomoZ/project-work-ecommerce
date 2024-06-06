4b9459708b10cc44a691a7e1d3cd173de50ee671
GiakomoZ <85363938+GiakomoZ@users.noreply.github.com>
2024-06-06 12:11:21 +0200

    Aggiunti i routerLink alla navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { ListaProdottiComponent } from './components/lista-prodotti/lista-prodotti.component';
+import { CarrelloComponent } from './components/carrello/carrello.component';
 
 export const routes: Routes = [
     { path: "", component: HomepageComponent },
     { path: "shop", component: ListaProdottiComponent },
+    {path:"carrello", component:CarrelloComponent},
     {path:"**", component:NotfoundComponent}
 ];
