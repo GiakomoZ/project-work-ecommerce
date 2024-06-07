4ea8e4c8a4109036058c083b4e73b764e1a6061e
GiakomoZ <85363938+GiakomoZ@users.noreply.github.com>
2024-06-07 10:35:58 +0200

    Aggiunto ritorno in cima al cambio pagina

--- src/app/components/lista-prodotti/lista-prodotti.component.ts
+++ src/app/components/lista-prodotti/lista-prodotti.component.ts
@@ -5,39 +5,45 @@ import { CommonModule } from '@angular/common';
 import { CardProdottoComponent } from '../card-prodotto/card-prodotto.component';
 
 @Component({
-    selector: 'app-lista-prodotti',
-    standalone: true,
-    imports: [CommonModule, CardProdottoComponent],
-    templateUrl: './lista-prodotti.component.html',
-    styleUrls: ['./lista-prodotti.component.css'],
+	selector: 'app-lista-prodotti',
+	standalone: true,
+	imports: [CommonModule, CardProdottoComponent],
+	templateUrl: './lista-prodotti.component.html',
+	styleUrls: ['./lista-prodotti.component.css'],
 })
 export class ListaProdottiComponent implements OnInit {
-    prodotti: Prodotto[] = [];
-    nProdotti: number = 0;
-    pages: number[] = [];
+	prodotti: Prodotto[] = [];
+	nProdotti: number = 0;
+	paginaCorrente: number = 0;
+	pages: number[] = [];
 
-    constructor(private productService: ProdottiService) { }
-    
-    loadProducts(page: number) {
-        this.productService
-            .getPaginatedProducts(page)
-            .subscribe((data: Prodotto[]) => {
-                this.prodotti = data;
-            });
-    }
+	constructor(private productService: ProdottiService) {}
 
-    loadPages() {
-        this.pages = [];  // Clear the array first
-        for(let i = 1; i <= Math.ceil(this.nProdotti / 10); i++) {
-            this.pages.push(i);
-        }
-    }
-    
-    ngOnInit(): void {
-        this.loadProducts(1);
-        this.productService.getProductsNumber().subscribe((data: number) => {
-            this.nProdotti = data;
-            this.loadPages();  // Call loadPages after nProdotti is set
-        });
-    }
+	loadProducts(page: number) {
+		if (page != this.paginaCorrente) {
+			this.productService
+				.getPaginatedProducts(page)
+				.subscribe((data: Prodotto[]) => {
+					this.prodotti = data;
+					this.paginaCorrente = page;
+
+			window.scrollTo(0, 0);
+				});
+		}
+	}
+
+	loadPages() {
+		this.pages = []; // Clear the array first
+		for (let i = 1; i <= Math.ceil(this.nProdotti / 10); i++) {
+			this.pages.push(i);
+		}
+	}
+
+	ngOnInit(): void {
+		this.loadProducts(1);
+		this.productService.getProductsNumber().subscribe((data: number) => {
+			this.nProdotti = data;
+			this.loadPages(); // Call loadPages after nProdotti is set
+		});
+	}
 }
