00c33b0663191b242190208b7c0756db09defef2
diego6 <giovanninidiego06@gmail.com>
2024-06-07 11:48:36 +0200

    info

---
+++ src/app/components/info-prodotto/info-prodotto.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProdottiService } from '../../services/prodotti.service';
+import { Prodotto } from '../../models/prodotto';
+import { CaroselloImmaginiComponent } from '../carosello-immagini/carosello-immagini.component';
+import { StelleRatingComponent } from '../stelle-rating/stelle-rating.component';
+
+
+@Component({
+  selector: 'app-info-prodotto',
+  standalone: true,
+  imports: [CommonModule, CaroselloImmaginiComponent, StelleRatingComponent],
+  templateUrl: './info-prodotto.component.html',
+  styleUrl: './info-prodotto.component.css'
+})
+export class InfoProdottoComponent {
+
+  @Input() id: number = 1;
+	@Input() titolo: string = '';
+	@Input() prezzo: number = 0;
+	@Input() immagini: string = '';
+	@Input() categoria: string = '';
+	@Input() rating: number = 0;
+  @Input() descrizione: string = '';
+
+
+	constructor (private productService: ProdottiService) 
+	{
+		this.productService.getProduct(this.id).subscribe((data: Prodotto) => {
+			this.titolo = data.title;
+			this.prezzo = data.price;
+			this.immagini = data.images;
+			this.categoria = data.category.name;
+			this.rating = data.stars;
+      this.descrizione = data.description;
+		});
+	}
+
+}
