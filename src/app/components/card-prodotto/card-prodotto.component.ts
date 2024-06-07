a54f7b28ee77a316346a0b008fe8f9d5276b221f
March222 <marcello.belli.stud@ispascalcomandini.it>
2024-06-07 16:52:42 +0200

    carrello quasi finito

--- src/app/components/card-prodotto/card-prodotto.component.ts
+++ src/app/components/card-prodotto/card-prodotto.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, ModelFunction } from '@angular/core';
 import { CaroselloImmaginiComponent } from '../carosello-immagini/carosello-immagini.component';
 import { StelleRatingComponent } from '../stelle-rating/stelle-rating.component';
 import { RouterModule } from '@angular/router';
+import { ProdottiService } from '../../services/prodotti.service';
+import { Prodotto } from '../../models/prodotto';
 
 @Component({
 	selector: 'app-card-prodotto',
@@ -18,4 +20,21 @@ export class CardProdottoComponent {
 	@Input() immagini: string = '';
 	@Input() categoria: string = '';
 	@Input() rating: number = 0;
+
+	elementoDaAggiungere: number=0;	
+	prodotto:Prodotto | undefined;
+
+	getId(id: number) {
+		this.elementoDaAggiungere=id;
+		console.log(this.elementoDaAggiungere);
+	}
+
+	constructor(prodottoservice:ProdottiService) 
+	{
+		prodottoservice.getProduct(this.elementoDaAggiungere).subscribe(r=>this.prodotto=r);
+	}
+
+	
 }
+
+
--- src/app/components/carrello/carrello.component.ts
+++ src/app/components/carrello/carrello.component.ts
@@ -16,15 +16,7 @@ export class CarrelloComponent {
   
   carrello: Carrello[] = [];
 
-
-  constructor() {
-    this.carrello = [
-      {nome: 1, img: 'https://store.inter.it/dw/image/v2/BJBQ_PRD/on/demandware.static/-/Sites-inter-master-catalog/default/dwd2bbc5a2/images/large/23N001M4003_02.jpg?sw=1400&q=80', descrizione: 'descrizione1', tipo: 'tipo1', prezzo: (30),quantita:1},
-      {nome: 2, img: 'https://store.acmilan.com/cdn/shop/products/765824-A18_01_800x.jpg?v=1657015003', descrizione: 'descrizione2', tipo: 'tipo2', prezzo: (20),quantita:2},
-      {nome: 3, img: 'https://static.sscnapoli.it/img/p/6/9/7/7/6977-home_default.jpg', descrizione: 'descrizione3', tipo: 'tipo3', prezzo: (30),quantita:3},
-    ];
-  }
-
+  
 
   
 
